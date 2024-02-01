6e5388249e45c6f14d69b9c59ae9698969f47af0
Pavlina Ivanova <pavlina33331988@gmail.com>
2024-02-01 16:57:32 +0200

    added
    added

---
+++ app.js
@@ -0,0 +1,35 @@
+﻿(function(){
+
+    let screen = document.querySelector('.screen');
+    let buttons = document.querySelectorAll('.btn');
+    let clear = document.querySelector('.btn-clear');
+    let equal =  document.querySelector('.btn-equal');
+
+    buttons.forEach((button)=>{
+        button.addEventListener('click',function(e){
+            //console.log(e.target);
+            //console.log(e.target.dataset.num);
+
+            let value = e.target.dataset.num;
+            screen.value += value;
+        })
+
+        
+    })
+
+    equal.addEventListener('click', equalS);
+    function equalS(){
+        if(screen.value === ''){
+            screen.value = "Please enter"
+        }else{
+            let answer = eval(screen.value);  // console.log(eval('2 + 2'))-> 4
+            screen.value = answer;
+        }
+    }
+     
+
+    clear.addEventListener('click', (e)=>{
+        screen.value = '';
+    })
+    
+})()
